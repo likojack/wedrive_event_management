835e6447564b1caaeaa168d6ceebd8f9f7172eaf
Michelle <lau.tsuiman@gmail.com>
2015-08-05 09:52:37 +1000

    FIrst Commit

---
+++ weDrive/ContactListViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ContactListViewController.swift
+//  weDrive
+//
+//  Created by kejielee on 27/07/2015.
+//  Copyright (c) 2015 michelle. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var people = ["one", "two", "three"]
+    var name : String = ""
+    var note : String = ""
+    var from : String = ""
+    var to : String = ""
+    var selectedCell : [Int] = []
+    var selectedPeople : [String] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.ContactListTableView.dataSource = self
+        self.ContactListTableView.delegate = self
+        
+        let backButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancelTapped:")
+        navigationItem.leftBarButtonItem = backButton
+        let rightButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "doneTapped:")
+        navigationItem.rightBarButtonItem = rightButton
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBOutlet weak var ContactListTableView: UITableView!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.people.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel?.text = people[indexPath.row]
+        return cell
+
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.selectedPeople.append(people[indexPath.row])
+    }
+    
+    func cancelTapped(sender: UIBarButtonItem) {
+        self.performSegueWithIdentifier("contactCancelSegue", sender: self)
+    }
+    func doneTapped(sender: UIBarButtonItem) {
+        self.performSegueWithIdentifier("peopleAddedSegue", sender: self)
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "peopleAddedSegue" {
+            var eventCreateViewController = segue.destinationViewController as CreateEventViewController
+            eventCreateViewController.people = self.selectedPeople
+            eventCreateViewController.name = self.name
+            eventCreateViewController.note = self.note
+            eventCreateViewController.from = self.from
+            eventCreateViewController.to = self.to
+            
+        }
+        
+    }
+
+}
---
+++ weDrive/EventDetailViewController.swift
@@ -0,0 +1,62 @@
+//
+//  EventDetailViewController.swift
+//  weDrive
+//
+//  Created by Michelle Lau on 21/07/2015.
+//  Copyright (c) 2015 michelle. All rights reserved.
+//
+
+import UIKit
+
+class EventDetailViewController: UIViewController {
+
+    var event : Event? = nil
+    var tappedImage : UIImage? = nil
+    
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    @IBOutlet weak var peopleLabel: UILabel!
+    
+    @IBOutlet weak var fromLabel: UILabel!
+    
+    @IBOutlet weak var toLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var printPeople : String = ""
+        for var i = 0; i < self.event?.people.count; i++ {
+            printPeople = printPeople + " " + self.event!.people[i]
+        }
+        
+        self.navigationItem.title = self.event!.name
+        self.previewImageView.image = UIImage(data: self.event!.previewimage)
+        self.fromLabel.text = self.event!.from
+        self.toLabel.text = self.event!.to
+        self.peopleLabel.text = printPeople
+        self.noteLabel.text = self.event!.note
+        self.timeLabel.text = self.event?.time
+        
+        var previewTapRecognizer = UITapGestureRecognizer(target: self, action: "previewTapped")
+        self.previewImageView.addGestureRecognizer(previewTapRecognizer)
+        
+    }
+    
+    func previewTapped() {
+        self.tappedImage = self.previewImageView.image
+        self.performSegueWithIdentifier("zoomSegue", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var zoomViewController = segue.destinationViewController as ZoomViewController
+        zoomViewController.image = self.tappedImage!
+    }
+
+
+
+
+}
